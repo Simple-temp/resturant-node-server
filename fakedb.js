efb1ce868a1cb152336a4bd1194899dc20834529
Simple-temp <fiveriam01@gmail..com>
2022-07-28 17:01:41 +0600

    some data change

--- fakedb.js
+++ fakedb.js
@@ -24,8 +24,28 @@ export const users = [
 
 export const foods = [
     {
-        name: "Burger3",
-        img: "https://ibb.co/Qk4ZydG",
+        name: "Homemade",
+        img: "https://res.cloudinary.com/image-hosting-api/image/upload/v1659005716/rs1_fz4yoy.png",
+        quantity: 1,
+        price: 10,
+        des: "Want to know where this information comes from? Learn more Images may be subject to copyright",
+        rating: 3.4,
+        review: 22,
+        stock: 3
+    },
+    {
+        name: "Noodles",
+        img: "https://res.cloudinary.com/image-hosting-api/image/upload/v1659005715/rs2_xznxz3.png",
+        quantity: 1,
+        price: 20,
+        des: "Want to know where this information comes from? Learn more Images may be subject to copyright",
+        rating: 3.4,
+        review: 22,
+        stock: 3
+    },
+    {
+        name: "Egg",
+        img: "https://res.cloudinary.com/image-hosting-api/image/upload/v1659005714/rs3_iulxiq.png",
         quantity: 1,
         price: 30,
         des: "Want to know where this information comes from? Learn more Images may be subject to copyright",
@@ -34,20 +54,20 @@ export const foods = [
         stock: 3
     },
     {
-        name: "Burger3",
-        img: "https://ibb.co/Qk4ZydG",
+        name: "Sushi Dizzy",
+        img: "https://res.cloudinary.com/image-hosting-api/image/upload/v1659005714/rs4_s48w22.png",
         quantity: 1,
-        price: 30,
+        price: 40,
         des: "Want to know where this information comes from? Learn more Images may be subject to copyright",
         rating: 3.4,
         review: 22,
         stock: 3
     },
     {
-        name: "Burger3",
-        img: "https://ibb.co/Qk4ZydG",
+        name: "The Coffee Break",
+        img: "https://res.cloudinary.com/image-hosting-api/image/upload/v1659005714/rs5_c1m07r.png",
         quantity: 1,
-        price: 30,
+        price: 40,
         des: "Want to know where this information comes from? Learn more Images may be subject to copyright",
         rating: 3.4,
         review: 22,
